2bb616cedd876a908c2ca69c9a14a7ed0a0970f4
Peter Pham <pttp15122002@gmail.com>
2025-02-14 09:06:47 +0700

    chore: install dependencies

---
+++ packages/galeon-core/.eslintrc.cjs
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:astro/recommended",
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
