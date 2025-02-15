216afa7e365bb3b0a21715d8c9beeee204bc5a28
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-02-15 12:34:42 +0700

    #OPS-003: generate routes (#8)
    * test: mock folder and files
    
    * ops: generate URLs from folders and files
    
    * test: generate routes function

--- packages/omniscroll-core/src/router.ts
+++ packages/omniscroll-core/src/router.ts
@@ -1,1 +1,9 @@
 //  # Generates routes dynamically
+import { glob } from "glob";
+
+export default function GetRoutes(baseDir: string) {
+    return glob.sync(`${baseDir}/**/*.{mdx, md}`).map((file) => {
+        const route = file.replace(baseDir, "").replace(/\\/g, "").replace(/\.(md|mdx)$/, "")
+        return { path: route, file }
+    })
+}
