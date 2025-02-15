73388e9e6051eb96fa63bf0b11c2b78b2de5b3d9
Peter Pham <89619124+Pet3r1512@users.noreply.github.com>
2025-02-15 16:41:58 +0700

    #OPS-006: default configs (#9)

--- packages/omniscroll-core/src/config.ts
+++ packages/omniscroll-core/src/config.ts
@@ -1,1 +1,17 @@
 // # Handles user configuration
+
+export interface OmniscrollConfig {
+    title: string,
+    sidebar?: {
+        label: string,
+        link: string
+    }
+}
+
+export const defaultConfig: OmniscrollConfig = {
+    title: "Omniscroll",
+    sidebar: {
+        label: "Default Label",
+        link: "Default Link"
+    }
+}
