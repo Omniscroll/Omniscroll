d68b163b01371610ea1e996acca958545a386fac
Peter Pham <pttp15122002@gmail.com>
2025-02-15 16:46:01 +0700

    ops: build html from markdown files

--- packages/omniscroll-core/src/build.ts
+++ packages/omniscroll-core/src/build.ts
@@ -1,1 +1,18 @@
 //  # Builds static files
+
+import ParseMarkdown from "./markdown";
+import GetRoutes from "./router";
+import fs from "fs-extra"
+
+export default function BuildSite(baseDir: string, outDir: string) {
+    const routes = GetRoutes(baseDir)
+
+    routes.forEach(({ path, file }) => {
+        const { frontmatter, content } = ParseMarkdown(file)
+        const outPath = `${outDir}/${path}.html`
+
+        fs.outputFileSync(outPath, `<h1>${frontmatter}</h1>\n${content}`)
+
+        console.log("Build Complete")
+    })
+}
