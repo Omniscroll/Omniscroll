91ccca43572e6be9e95ba87af2d39941973e7df2
Peter Pham <pttp15122002@gmail.com>
2025-02-15 17:07:52 +0700

    fix: backslashes

--- packages/omniscroll-core/src/build.ts
+++ packages/omniscroll-core/src/build.ts
@@ -1,18 +1,18 @@
-//  # Builds static files
-
 import ParseMarkdown from "./markdown";
 import GetRoutes from "./router";
-import fs from "fs-extra"
+import fs from "fs-extra";
+import path from "path";
 
 export default function BuildSite(baseDir: string, outDir: string) {
-    const routes = GetRoutes(baseDir)
+    const routes = GetRoutes(baseDir);
 
-    routes.forEach(({ path, file }) => {
-        const { frontmatter, content } = ParseMarkdown(file)
-        const outPath = `${outDir}/${path}.html`
+    routes.forEach(({ path: routePath, file }) => {
+        const { frontmatter, content } = ParseMarkdown(file);
 
-        fs.outputFileSync(outPath, `<h1>${frontmatter}</h1>\n${content}`)
+        const outPath = path.posix.join(outDir, `${routePath}.html`);
 
-        console.log("Build Complete")
-    })
+        fs.outputFileSync(outPath, `<h1>${frontmatter.title}</h1>\n${content}`);
+
+        console.log("Build Complete:", outPath);
+    });
 }
