6146a558eeea2a3334734c15cc888292cf4dd2fa
Peter Pham <pttp15122002@gmail.com>
2025-03-03 14:38:40 +1100

    imp: reading markdown files and generate routes

--- packages/omniscroll-core/src/router.ts
+++ packages/omniscroll-core/src/router.ts
@@ -1,12 +1,26 @@
 //  # Generates routes dynamically
 import { glob } from "glob";
+import path from "path";
+import fs from "fs-extra";
+import matter from "gray-matter";
 
 export default function GetRoutes(baseDir: string) {
     return glob.sync(`${baseDir}/**/*.{mdx, md}`).map((file) => {
-        const route = file
-            .replace(baseDir, "")
-            .replace(/\\/g, "")
-            .replace(/\.(md|mdx)$/, "");
-        return { path: route, file };
+        const relativePath = path.relative(baseDir, file);
+        let route = relativePath
+            .replace(/\\/g, "/")
+            .replace(/\.(md|mdx)$/, "")
+            .replace(/index$/, ""); // compile index file to be root page "/"
+
+        const fileContent = fs.readFileSync(file, "utf-8");
+        const { data } = matter(fileContent);
+
+        if (data.slug) {
+            route = data.slug.startsWith("/") ? data.slug : `/${data.slug}`;
+        } else {
+            route = `/${data.slug}`;
+        }
+
+        return { path: `/${route}`, file };
     });
 }
